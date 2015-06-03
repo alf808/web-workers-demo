12c592ac12a38b36a0cb51138c36d0b8ea738a86
alf <tewada@charlies-MacBook-Air.local>
2015-06-03 21:17:22 +0700

    revised fork of udacity's web worker
    i changed image-app.js to use the Worker class to hand off processing
    to another thread while main thread continues its processing

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -6,6 +6,9 @@
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
 
+  //create a Worker instance
+  var webWorker = new Worker('scripts/worker.js');
+
   function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -43,20 +46,14 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
+    // send message to web worker
+    webWorker.postMessage({'imageData': imageData, 'type': type});
+
+    // receive message from worker
+    webWorker.onmessage = function(e) {
     toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    return ctx.putImageData(e.data, 0, 0);
+    }
   };
 
   function revertImage() {
